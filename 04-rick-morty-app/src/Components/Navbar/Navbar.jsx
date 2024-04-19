fdeb681b4d479e5fc529ada9be41d23f5f57f980
jesualujan <lujan.oficial@gmail.com>
2024-04-18 21:34:00 -0600

    ✨ Creamos nuestro componente Navbar

--- 04-rick-morty-app/src/Components/Navbar/Navbar.jsx
+++ 04-rick-morty-app/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-
+import './navbar.css'
+import logo from '../../assets/react.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <nav className='navbar navbar-dark bg-dark'>
+        <a className='navbar__link' href='/'> RICK AND MORTY APP</a>
+        <div className='navbar__container-fluid'>
+          <img className='navbar__logo' src={logo} alt='logo__react' />
+        </div>
+      </nav>
+    </>
   )
 }
 
