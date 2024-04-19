0f732a4552a5c040b80790adf302f5f89626ef1a
jesualujan <lujan.oficial@gmail.com>
2024-04-18 20:18:04 -0600

    ✨ importamos nuestros componentes

---
+++ 04-rick-morty-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Character from './Components/Character/Character'
+import PagButtons from './Components/PagButtons/PagButtons'
+function App() {
+  
+
+  return (
+    <>
+     <Navbar />
+     <Character />
+     <PagButtons />
+     <h1> FUNCIONA 😃</h1>
+    </>
+  )
+}
+
+export default App
