3bb6bd808e7d9474858c9b367ee8e14e94bb47c1
jesualujan <lujan.oficial@gmail.com>
2024-04-18 21:33:23 -0600

    ✨ corrección de sintaxis con standard

--- 04-rick-morty-app/src/main.jsx
+++ 04-rick-morty-app/src/main.jsx
@@ -7,9 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // Bootstrap Bundle JS
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
