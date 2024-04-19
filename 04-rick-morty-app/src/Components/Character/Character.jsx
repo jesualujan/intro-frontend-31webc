2d84a192b15223fdccda5fd40a398d163afd6bc3
jesualujan <lujan.oficial@gmail.com>
2024-04-18 21:35:08 -0600

    🚧 en creación el componente Character

--- 04-rick-morty-app/src/Components/Character/Character.jsx
+++ 04-rick-morty-app/src/Components/Character/Character.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable react/prop-types */
+import './character.css'
 
-
-const Character = () => {
+const Character = ({ characterList }) => {
   return (
-    <div>Character</div>
+    <div className='container custom-container'>
+      <div className='row'>
+        {
+                        /* Transformar la lista de personajes */
+                        characterList.map((item, index) => (
+                          /* Codigo html genérico de las tarjetas */
+                          <div key={index} className='col-lg-3 col-md-6 col-sm-12 mb-4'>
+      
+                          </div>
+                        ))
+                    }
+      </div>
+    </div>
   )
 }
 
