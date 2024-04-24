d46e1065c08d320ffb1baf69fb1319649f7a3bc1
jesualujan <lujan.oficial@gmail.com>
2024-04-23 19:51:44 -0600

    ✨ creamos la card para nuestros personajes

--- 04-rick-morty-app/src/Components/Character/Character.jsx
+++ 04-rick-morty-app/src/Components/Character/Character.jsx
@@ -9,8 +9,19 @@ const Character = ({ characterList }) => {
                         /* Transformar la lista de personajes */
                         characterList.map((item, index) => (
                           /* Codigo html genérico de las tarjetas */
-                          <div key={index} className='col-lg-3 col-md-6 col-sm-12 mb-4'>
-      
+                          <div className='card col-lg-3 col-md-6 col-sm-12 mb-4' key={index}>
+                            <div className='card_container' style={{ minWidth: '300px' }}>
+                              <img className='card-img-top' src={item.image} alt={item.name} />
+                              <div>
+                                <h2 className='card-title'>{item.name}</h2>
+                                <h3 className='card-title'>ID: {item.id}</h3>
+                                <hr />
+                                <p className='card-text'>Species: {item.species}</p>
+                                <p className='card-text'>Gender: {item.gender}</p>
+                                <p className='card-text'>Location: {item.location.name}</p>
+                                <p className='card-text'>Status: {item.status}</p>
+                              </div>
+                            </div>
                           </div>
                         ))
                     }
